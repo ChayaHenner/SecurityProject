1f59ec571767ce38a291991a95462cd4f10814da
ChayaHenner <chayahenner@gmail.com>
2024-01-30 21:27:11 +0200

    now

--- server_auth/response.py
+++ server_auth/response.py
@@ -1,26 +1,25 @@
 import struct
-client_request = {
-    1025: "RegisterClient",
-    1027: "RegisterServer",
-    1026: "GetServerList",
-    1027: "GetSymmetricKey",
-}#fix 1027
+server_response = {
+    1600: "RegisterSuccess",
+    1601: "RegisterFail",
+    1602: "ServerList",
+    1603: "SendSymmetricKey",
+}
 
-class Request:
-    def __init__(self,clientID,version,code,payload_size,payload):
-        self.clientID= clientID
+class Response:
+    def __init__(self,version,code,payload_size,payload):
         self.version=version
         self.code=code
         self.payload_size=payload_size
         self.payload=payload
 
     def pack(self):
-        packed_data = struct.pack(f"!16s B H I {len(self.payload)}s",
-                                  self.clientID.encode('utf-8'),
+        packed_data = struct.pack(f" B H I {len(self.payload)}s",
                                   self.version,
                                   self.code,
                                   self.payload_size,
                                   self.payload.encode('utf-8'))
         return packed_data
+    
 
         
--- server_auth/ticket.py
+++ server_auth/ticket.py
@@ -1,21 +1,17 @@
 import struct
-client_request = {
-    1025: "RegisterClient",
-    1027: "RegisterServer",
-    1026: "GetServerList",
-    1027: "GetSymmetricKey",
-}#fix 1027
 
-class Request:
-    def __init__(self,clientID,version,code,payload_size,payload):
+class Ticket:
+    def __init__(self,clientID,serverID,ticketIV,version,code,payload_size,payload):
         self.clientID= clientID
+        self.serverID= serverID
+        self.ticketIV= ticketIV
         self.version=version
         self.code=code
         self.payload_size=payload_size
         self.payload=payload
 
     def pack(self):
-        packed_data = struct.pack(f"!16s B H I {len(self.payload)}s",
+        packed_data = struct.pack(f"!16s !16s !16s B H I {len(self.payload)}s",
                                   self.clientID.encode('utf-8'),
                                   self.version,
                                   self.code,
